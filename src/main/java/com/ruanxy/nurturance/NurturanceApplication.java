452372dc77196d00d9ff426d024ff991e84a7a18
ruanxiangyu <1306746563@qq.com>
2023-01-11 10:56:27 +0800

    Initial commit

---
+++ src/main/java/com/ruanxy/nurturance/NurturanceApplication.java
@@ -0,0 +1,13 @@
+package com.ruanxy.nurturance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurturanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurturanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruanxy/nurturance/NurturanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruanxy.nurturance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurturanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
